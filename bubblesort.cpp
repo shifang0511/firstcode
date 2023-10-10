6d812e57760ad6850d713a4df4d869c5147588f0
shifang0511 <lizhongyu2005@gmail.com>
2023-10-10 17:23:13 +0800

    bubble

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 #include<string>
 using namespace std;
+void swap(double &a,double &b)
+{
+    double temp = a;
+    a = b;
+    b = temp;
+}
+void bubblesort(double *arr,int length)
+{
+    for (int i = 0; i < length-1 ;i++)
+        for (int j = 0; j < length - i-1;j++)
+        {
+        if(arr[j]>arr[j+1])
+            swap(arr[j], arr[j + 1]);
+        }
+}
 int main()
 {
-    cout << "hello world" << endl;
-    system("pause");
+    double arr[]={5,2,8,1,3};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    bubblesort(arr, length);
+    for (int i = 0; i < length;i++)
+        cout << arr[i] << " ";
+        system("pause");
     return 0;
 }
