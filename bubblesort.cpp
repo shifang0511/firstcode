4a1d170af0d2d864b9e7038119e2cfeee586f269
shifang0511 <lizhongyu2005@gmail.com>
2024-03-18 19:10:44 +0800

    Refactor bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<string>
-<<<<<<< HEAD
 #include<algorithm>
 #include<iomanip>
 #include<vector>
@@ -30,31 +29,4 @@ int main() {
 	quicksort(q,0,n-1);
 	for(int i=0;i<n;i++)printf("%d ",q[i]);
 	system("pause");
-=======
-using namespace std;
-void swap(double &a,double &b)
-{
-    double temp = a;
-    a = b;
-    b = temp;
-}
-void bubblesort(double *arr,int length)
-{
-    for (int i = 0; i < length-1 ;i++)
-        for (int j = 0; j < length - i-1;j++)
-        {
-        if(arr[j]>arr[j+1])
-            swap(arr[j], arr[j + 1]);
-        }
-}
-int main()
-{
-    double arr[]={5,2,8,1,3};
-    int length = sizeof(arr) / sizeof(arr[0]);
-    bubblesort(arr, length);
-    for (int i = 0; i < length;i++)
-        cout << arr[i] << " ";
-        system("pause");
-    return 0;
->>>>>>> 6d812e57760ad6850d713a4df4d869c5147588f0
 }
